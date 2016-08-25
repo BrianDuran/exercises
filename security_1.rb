9347f50378e557bb69a8534af36b642fab9b6e60
BrianDuran <bduran@growthaccelerationpartners.com>
2016-08-24 20:30:39 -0600

    Update security_1.rb

--- security_1.rb
+++ security_1.rb
@@ -1,7 +1,20 @@
 # Case 1
 login = params[:login]
 @users = User.where("login = #{login}")
+=begin
+@user = User.where('login = ?', params[:login])
+
+Posible ataque
+DELETE
+
+id = '1 OR 1=1'
+delete from User where id = 1 OR 1=1
+=end
+
+#------------------------------
 
 # Case 2
 User.destroy_all("name like '" + param[:name] + "'")
+
+# User.destroy_all('name like ?', param[:name])
 
