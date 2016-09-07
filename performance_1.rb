546c9bc3291d1cfac9e41772ddad5aa891edb35a
BrianDuran <bduran@growthaccelerationpartners.com>
2016-09-06 20:49:06 -0600

    Update performance_1.rb

--- performance_1.rb
+++ performance_1.rb
@@ -16,3 +16,25 @@ def print_users
     puts "#{user.id} - #{user.name} - #{user.role.name}"
   end
 end
+
+
+#---------------------------------------------------------------
+
+# app/models/user.rb
+class User < ActiveRecord::Base
+  belongs_to :role
+end
+
+# app/models/role.rb
+class Role < ActiveRecord::Base
+  has_many :users
+end
+
+###############
+
+def print_users
+  users = User.includes(:roles).all # There are 102 users
+  users.each do |user|
+    puts "#{user.id} - #{user.name} - #{user.role.name}"
+  end
+end
